2b7a28af7a47af0574fce2b46f073be28c2e5c29
Eric Bollinger <ericsbollinger@gmail.com>
2017-12-24 21:50:54 -0800

    Solved day 8. Fun one.
    Signed-off-by: Eric Bollinger <ericsbollinger@gmail.com>

---
+++ Day8/second.py
@@ -0,0 +1,49 @@
+def getRegValue(x):
+    if (x not in regs.keys()):
+        regs[x] = 0
+        return 0
+    return regs[x]
+
+def runTest(reg, op, num):
+    if (op == '>'):
+        return (getRegValue(reg) > num)
+    elif (op == '<'):
+        return (getRegValue(reg) < num)
+    elif (op == '>='):
+        return (getRegValue(reg) >= num)
+    elif (op == '<='):
+        return (getRegValue(reg) <= num)
+    elif (op == '=='):
+        return (getRegValue(reg) == num)
+    elif (op == '!='):
+        return (getRegValue(reg) != num)
+
+with open('input.txt') as f:
+    regs = {}
+
+    lines = list(f.readlines())
+    lines = [x.strip() for x in lines]
+
+    big = 0
+
+    for l in lines:
+        items = l.split()
+        cond = items[4]
+        op = items[5]
+        num = int(items[6])
+
+        if (runTest(cond, op, num)):
+            reg = getRegValue(items[0])
+            if (items[1] == 'inc'):
+                reg += int(items[2])
+            else:
+                reg -= int(items[2])
+            regs[items[0]] = reg
+
+            if (reg > big):
+                big = reg
+
+        else:
+            continue
+
+    print big
